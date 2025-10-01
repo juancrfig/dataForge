36d99248cc759c7e2bd9119d3dea765de316c842
juancrfig <juancrfig@gmail.com>
2025-10-01 12:16:09 -0500

    Add handler function for sanitizing strings

--- src/migration/main.py
+++ src/migration/main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-import os
+import os, re
 import pandas as pd
 from typing import List, Dict, Any
 
 load_dotenv() # Reads .env and adds to environment
+
 
 TABLES_CONFIG: List[Dict[str, Any]] = [
     {'table_name': 'customers', 'file_path': 'bronze/olist_customers_dataset.csv'},
@@ -16,6 +17,19 @@ TABLES_CONFIG: List[Dict[str, Any]] = [
     {'table_name': 'sellers', 'file_path': 'bronze/olist_sellers_dataset.csv'},
     {'table_name': 'product_category_name_translation', 'file_path': 'bronze/product_category_name_translation.csv'}
 ]
+
+
+def _sanitize_strings(s: str) -> str:
+    """
+    Strips dangerous characters used in common injection attacks from a string.
+    This is a basic, proactive security measure for raw data strings.
+    """
+    if pd.isna(s) or not isinstance(s, str):
+        return s
+
+    s = re.sub(r'[";\'`()\[\]\{\}<>\-\-\#]', '', s)
+    return s
+
 
 def extract_data(tables_config: List[Dict[str, Any]]) -> Dict[str,  pd.DataFrame]:
     """
@@ -49,6 +63,33 @@ def extract_data(tables_config: List[Dict[str, Any]]) -> Dict[str,  pd.DataFrame
 
 def transform_data(extracted_data: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
     pass
+
+
+def transform_customers(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Applies Silver Layer cleaning and type optimization to the customer DataFrame.
+
+    This process ensures categorical identifier columns are stored using the memory-efficient 'category' dtype,
+    which significantly improves performance for grouping and merging operations later in the pipeline.
+
+    Args:
+        df: The raw customer DataFrame.
+
+    Returns:
+        The transformed customer DataFrame with optimized dtypes.
+    """
+    # 1. Type Optimization  for Categorical Identifiers
+    for col in ['customer_zip_code_prefix', 'customer_city', 'customer_state']:
+        if col in df.columns:
+            df[col] = df[col].astype('category')
+
+    # 2. Standardization / Cleaning
+    if 'customer_city' in df.columns:
+        df['customer_city'] = df['customer_city'].str.lower().str.strip()
+
+    # 3. Handle potential null values, although not required for this dataset.
+
+    return df
 
 
 def load_data(transformed_data: Dict[str, pd.DataFrame]):
